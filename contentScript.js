fba96e81f9609f8552308ef92d7692c19acae10c
Daniel Brubaker <dwbrub@gmail.com>
2019-06-03 10:51:45 -0500

    tryin some new ways to set darkmode

--- contentScript.js
+++ contentScript.js
@@ -1,53 +1,55 @@
-
 var share = document.getElementsByClassName("docs-titlebar-buttons");
 var lastEdit = document.getElementsByClassName("docs-title-save-label");
-var material = document.getElementsByClassName("docs-material"); //used for topbar, outline, etc.  AN ARRAY
+var material = document.getElementsByClassName("docs-material");            //Used for topbar, outline, etc.  
 var toolbar = document.getElementById("docs-toolbar-wrapper");
 var titleBar = document.getElementsByClassName("docs-title-outer");
 var pageBackground = document.getElementsByClassName("kix-appview-editor");
-var pageMid = document.getElementsByClassName("kix-page-column"); // AN ARRAY
-var headersFooters = document.getElementsByClassName("kix-print-block"); // AN ARRAY
-var menuButton = document.getElementsByClassName("menu-button"); // AN ARRAY
-var menuItem = document.getElementsByClassName("goog-menuitem"); // AN ARRAY
+var pageMid = document.getElementsByClassName("kix-page-column");           
+var headersFooters = document.getElementsByClassName("kix-print-block");    
+var menuButton = document.getElementsByClassName("menu-button");            
+var menuItem = document.getElementsByClassName("goog-menuitem");            
+var blockButtons = document.getElementsByClassName("goog-inline-block");    //Used for undo, redo, and other menubar buttons //CHANGE CAUSE IT GETS STUFF YA DONT WANT
+var docTitle = document.getElementsByClassName("docs-title-input");
+var ruler = document.getElementsByClassName("docs-ruler");
+var rulerInner = document.getElementsByClassName("docs-ruler-face");
+
+
+
 var foregroundColor = "#4d4d4d";
 var backgroundColor = "#333333";
 
 var background = [pageBackground, share, lastEdit, material, toolbar, titleBar, menuItem];
-var foreground = [pageMid, headersFooters, menuButton];
+var foreground = [pageMid, headersFooters, menuButton, blockButtons, docTitle, ruler, rulerInner];
 
 
 function colors(input, setColor) {
     for (i = 0; i < input.length; i++) {
-        for (j = 0; j < input[i].length; j++) {
-            console.log(i + ":   " + j);
+        if (input[i].length == undefined) {
+            input[i].style.backgroundColor = setColor;
+            input[i].style.color = "White"
+        } else {
+            for (j = 0; j < input[i].length; j++) {
 
-            input[i][j].style.backgroundColor = setColor;
-            input[i][j].style.color = "White";
-
+                input[i][j].style.backgroundColor = setColor;
+                input[i][j].style.color = "White";
+            }
         }
     }
 
     return true;
 }
 
+window.onmousemove() = function () { colors(foreground, foregroundColor); }
+window.onmousemove() = function () { colors(background, backgroundColor); }
 
+colors(foreground, foregroundColor);
+colors(background, backgroundColor);
 
-
-window.onload(colors(foreground, foregroundColor), colors(background, backgroundColor));
-
-
-//      |    TEMPORARY BAD SOLUTION      |
-//      V                                V
-//while(!document.body.contains("docs-explore-widget-button")) {
-//    colors(foreground, foregroundColor);
-//    colors(background, backgroundColor);
-//}
 
 
 
 //
 // TODO:
-//  - check dif between a class and an id and adjust accordingly
 //  - constant check current page count v last page count
 //  - after check, run colors func again
 //  - get a frickin graphic designer
